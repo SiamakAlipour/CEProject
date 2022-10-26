5053def9baddda79547e1cbc96c56cdeb3f91a0d
Siamak Alipour <siamakalipour37@gmail.com>
2022-10-26 19:09:57 +0330

    new header added

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
 
 import './Header.scss';
 
 function Header() {
 	return (
 		<div className='header'>
-			{/* Navbar */}
-			<ul className='header__navBar'>
-				<li>
-					<a href=''>تست</a>
-				</li>
-				<li>
-					<a href=''>تست</a>
-				</li>
-				<li>
-					<a href=''>تست</a>
-				</li>
-			</ul>
-			{/* Login Menu */}
-			<ul className='header__loginMenu'>
-				<li>
-					<Link to='/login'>ورود</Link>
-				</li>
-				<li>
-					<Link to='/register' className='active'>
-						ثبت نام
-					</Link>
-				</li>
-			</ul>
+			<div className='header__wrapper'>
+				<h1 className='header__logo | fw-bold fs-primary-heading'>Shop</h1>
+				<nav className='header__navigation'>
+					<ul>
+						<li>
+							<a href='#'>Categories</a>
+						</li>
+						<li>
+							<a href='#'>Contact</a>{' '}
+						</li>
+						<li>
+							<a href='#'>About</a>
+						</li>
+						<li>
+							<a href='#'>Search</a>{' '}
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div className='header__panel'>
+				<ShoppingCartIcon />
+				<PersonIcon />
+			</div>
 		</div>
 	);
 }
