ff7537125f72d22800058c73edb701e2f4f98865
Siamak Alipour <siamakalipour37@gmail.com>
2022-12-31 07:59:21 +0330

    App updated

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 import Header from './Header';
 import Slideshow from './Slideshow';
+import HeadCarousel from './HeadCarousel';
 
 import './App.scss';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
 	return (
 		<div className='app'>
 			<Header />
 			{/* <Slideshow /> */}
+			<HeadCarousel />
 			<Slideshow />
-			<Slideshow />
-			<Slideshow />
+			<Main />
+			<Footer />
 		</div>
 	);
 }
