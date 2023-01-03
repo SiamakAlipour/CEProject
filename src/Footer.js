13336f47104cf08baf7e42cd68df538ec9b71b38
Siamak Alipour <siamakalipour37@gmail.com>
2023-01-03 12:45:49 +0330

    some changes done

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,20 @@
+// imports
 import React from 'react';
+
+// 
+import FacebookIcon from '@mui/icons-material/Facebook';
+
 import './Footer.scss';
 
+
 const Footer = () => {
-	return <div className='footer'></div>;
+	return <div className='footer'>
+		<div className="footer__navigator">
+			<div className="footer__socials"></div>
+			<div className="footer_nav"></div>
+		</div>
+		<div className="footer__copyright"></div>
+	</div>;
 };
 
 export default Footer;
