c8bdcc746f8f62c30e440c3d1bff9774b276b426
Siamak Alipour <siamakalipour37@gmail.com>
2023-01-03 16:05:36 +0330

    Footer done

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,44 @@
 // imports
 import React from 'react';
 
-// 
+//
 import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 import './Footer.scss';
 
-
 const Footer = () => {
-	return <div className='footer'>
-		<div className="footer__navigator">
-			<div className="footer__socials"></div>
-			<div className="footer_nav"></div>
+	return (
+		<div className='footer'>
+			<div className='footer__navigator'>
+				<div className='footer__socials'>
+					<InstagramIcon />
+					<WhatsAppIcon />
+					<FacebookIcon />
+				</div>
+				<div className='footer__nav'>
+					<ul>
+						<li>
+							<a href=''>Categories</a>
+						</li>
+						<li>
+							<a href=''>Contact</a>
+						</li>
+						<li>
+							<a href=''>About</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className='footer__copyright'>
+				&copy; Created by Siamak Alipour -{' '}
+				<a href='https://github.com/SiamakAlipour/CEProject' target='_blank'>
+					Github
+				</a>
+			</div>
 		</div>
-		<div className="footer__copyright"></div>
-	</div>;
+	);
 };
 
 export default Footer;
